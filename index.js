a4b2faa54a8b1997d738ca4bcc43ffa2be7467fe
Chris <abody.97@gmail.com>
2014-11-19 14:25:50 -0700

    Initial index.js and npm package stuff

---
+++ index.js
@@ -0,0 +1,27 @@
+module.exports = function() {
+	var queue = [], offset = 0;
+	this.__defineGetter__("length", function() {
+		return queue.length - offset;
+	});
+	this.__defineGetter__("top", function() {
+		return (queue.length > 0 ? queue[offset] : undefined);
+	});
+	this.push = function(val) {
+		queue.push(val);
+	};
+	this.pop = function() {
+		if(queue.length === 0) {
+			return undefined;
+		}
+		var old = queue[offset];
+		offset ++;
+		if(offset * 2 >= queue.length) {
+			queue = queue.slice(offset);
+			offset = 0;
+		}
+		return old;
+	};
+	this.reset = function() {
+		queue = [];
+	};
+};
